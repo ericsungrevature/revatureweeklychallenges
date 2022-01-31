b68f328d7865be925dbda975a73efbbc05c23968
ericsungrevature <93632639+ericsungrevature@users.noreply.github.com>
2022-01-31 13:10:19 -0500

    Create week01312022.js

---
+++ week01312022.js
@@ -0,0 +1,16 @@
+//https://connect.revature.net/stream/340385000004894042
+//Problem 1: Filter Out Strings from an Array
+function filterArray(arr) {
+	return arr.filter(element => {typeof element != "string"});
+}
+
+//Problem 2: Multiply by 11
+function multiplyBy11(str){
+	return str * "11"
+}
+
+/*
+Problem 3: Logical Reasoning Question:
+Switch on one of the light switches then go to the adjacent room to check the light bulbs to see which light the switch controlled.
+Go back and then turn off the light switch. Repeat the steps for the other light switches to find which switch controls which light.
+*/
